aea2a006c460c5046b0d394deca07555b6f5bb16
jinhu zhang <814030966@qq.com>
2018-12-26 16:20:49 +0800

    no message

--- JHMediator/JHMediator/Class/UIApplication+GetRootVC.m
+++ JHMediator/JHMediator/Class/UIApplication+GetRootVC.m
@@ -20,18 +20,46 @@
 
 #pragma mark - 获取当前显示的VC
 - (UIViewController *) getCurrentViewControllerFrom:(UIViewController *) vc {
-    if([vc isKindOfClass:[UISplitViewController class]]) {
-        return [self getCurrentViewControllerFrom:((UISplitViewController*) vc).viewControllers.lastObject];
-    } else if ([vc isKindOfClass:[UINavigationController class]]) {
-        return [self getCurrentViewControllerFrom:[((UINavigationController *) vc) visibleViewController]];
-    } else if ([vc isKindOfClass:[UITabBarController class]]) {
-        return [self getCurrentViewControllerFrom:[((UITabBarController *) vc) selectedViewController]];
-    } else {
-        if (vc.presentedViewController) {
-            return [self getCurrentViewControllerFrom:vc.presentedViewController];
-        } else {
+//    if([vc isKindOfClass:[UISplitViewController class]]) {
+//        return [self getCurrentViewControllerFrom:((UISplitViewController*) vc).viewControllers.lastObject];
+//    } else if ([vc isKindOfClass:[UINavigationController class]]) {
+//        return [self getCurrentViewControllerFrom:[((UINavigationController *) vc) visibleViewController]];
+//    } else if ([vc isKindOfClass:[UITabBarController class]]) {
+//        return [self getCurrentViewControllerFrom:[((UITabBarController *) vc) selectedViewController]];
+//    } else {
+//        if (vc.presentedViewController) {
+//            return [self getCurrentViewControllerFrom:vc.presentedViewController];
+//        } else {
+//            return vc;
+//        }
+//    }
+    if (vc.presentedViewController) {
+        // Return presented view controller
+        return [self getCurrentViewControllerFrom:vc.presentedViewController];
+    } else if( [vc isKindOfClass:[UISplitViewController class]]) {
+        // Return right hand side
+        UISplitViewController *svc = (UISplitViewController*) vc;
+        if (svc.viewControllers.count > 0)
+            return [self getCurrentViewControllerFrom:svc.viewControllers.lastObject];
+        else
             return vc;
-        }
+    } else if ([vc isKindOfClass:[UINavigationController class]]) {
+        // Return top view
+        UINavigationController *svc = (UINavigationController*) vc;
+        if (svc.viewControllers.count > 0)
+            return [self getCurrentViewControllerFrom:svc.topViewController];
+        else
+            return vc;
+    } else if ([vc isKindOfClass:[UITabBarController class]]) {
+        // Return visible view
+        UITabBarController *svc = (UITabBarController*) vc;
+        if (svc.viewControllers.count > 0)
+            return [self getCurrentViewControllerFrom:svc.selectedViewController];
+        else
+            return vc;
+    } else {
+        // Unknown view controller type, return last child view controller
+        return vc;
     }
 }
 - (UINavigationController *)currentNavigationController {
